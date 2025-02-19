68f2df37dd200d0c0fe196b9e773b2f89e77833d
kayqueR69 <kayquericharlyson@gmail.com>
2025-02-19 08:22:54 -0300

    add: commit inicial

---
+++ src/main/java/com/ifscsal/clinicaodontologica/clinicaodotologica/ClinicaodotologicaApplication.java
@@ -0,0 +1,13 @@
+package com.ifscsal.clinicaodontologica.clinicaodotologica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaodotologicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaodotologicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ifscsal/clinicaodontologica/clinicaodotologica/ClinicaodotologicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifscsal.clinicaodontologica.clinicaodotologica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaodotologicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
