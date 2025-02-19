254dba5685557085403022eb1d6644fa91751958
kayqueR69 <kayquericharlyson@gmail.com>
2025-02-19 10:15:37 -0300

    add: adição do endpoint de login

--- src/main/java/com/ifscsal/clinicaodontologica/clinicaodotologica/controller/ClienteController.java
+++ src/main/java/com/ifscsal/clinicaodontologica/clinicaodotologica/controller/ClienteController.java
@@ -1,5 +1,6 @@
 package com.ifscsal.clinicaodontologica.clinicaodotologica.controller;
 
+import com.ifscsal.clinicaodontologica.clinicaodotologica.DTO.ClienteDTO;
 import com.ifscsal.clinicaodontologica.clinicaodotologica.model.DAO.ICliente;
 import com.ifscsal.clinicaodontologica.clinicaodotologica.model.entidades.Cliente;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping ("/cliente")
@@ -22,5 +27,30 @@ public class ClienteController {
         Cliente novoCliente = dao.save(cliente);
 
         return ResponseEntity.status(201).body(novoCliente);
+    }
+
+    @PostMapping ("/login")
+    public ResponseEntity<Map> loginCliente (@RequestBody ClienteDTO cliente) {
+
+        ArrayList<Cliente> clientes = (ArrayList<Cliente>) dao.findAll();
+
+        Map<String, Object> resposta = new HashMap<>();
+
+        for (int c = 0; c < clientes.size(); c++) {
+
+            if (clientes.get(c).getEmail().equalsIgnoreCase(cliente.getEmail()) &&  clientes.get(c).getSenha().equals(cliente.getSenha())) {
+                resposta.put("Menssagem", "Logado no sistema!");
+
+                return ResponseEntity.status(200).body(resposta);
+            } else {
+                resposta.put("Menssagem", "Login ou senha invalido!");
+
+                return ResponseEntity.status(401).body(resposta);
+            }
+
+        }
+
+        resposta.put("Menssagem", "Bad request!");
+        return ResponseEntity.status(400).body(resposta);
     }
 }
