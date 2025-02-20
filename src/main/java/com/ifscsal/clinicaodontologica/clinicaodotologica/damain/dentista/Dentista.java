78a747d040172e97dec75b5af34f1a81851bdc9e
luiz <luiz.felipe7@aluno.ifsertao-pe.edu.br>
2025-02-20 15:26:04 -0300

    Aplica na entidade a anotação @Column, para uma boa pratica na programação

--- src/main/java/com/ifscsal/clinicaodontologica/clinicaodotologica/damain/dentista/Dentista.java
+++ src/main/java/com/ifscsal/clinicaodontologica/clinicaodotologica/damain/dentista/Dentista.java
@@ -2,28 +2,35 @@ package com.ifscsal.clinicaodontologica.clinicaodotologica.damain.dentista;
 
 import com.ifscsal.clinicaodontologica.clinicaodotologica.DTO.DentistaDTO;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+
+import lombok.*;
 
 @Table(name = "dentista")
 @Entity
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode(of = "id")
 public class Dentista{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    private String nome;
-    private String especialidade;
-    private String email;
-    private String senha;
-    private String CRO;
 
-    public Dentista() {}
+    @Column(name = "nome", length = 50)
+    private String nome;
+
+    @Column(name = "especialidade", length = 50)
+    private String especialidade;
+
+    @Column(name = "email", length = 200, unique = true)
+    private String email;
+
+    @Column(name = "senha", length = 200)
+    private String senha;
+
+    @Column(name = "cro", length = 50)
+    private String CRO;
 
     public Dentista(DentistaDTO dentistaDTO) {
         this.nome = dentistaDTO.nome();
