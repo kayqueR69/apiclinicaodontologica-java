f01364d81adad1837179e10629562e3eafd7efa9
kayqueR69 <kayquericharlyson@gmail.com>
2025-02-20 13:36:11 -0300

    wip: alterações no endpoint de agendamento de consultas

--- src/main/java/com/ifscsal/clinicaodontologica/clinicaodotologica/controller/ConsultaController.java
+++ src/main/java/com/ifscsal/clinicaodontologica/clinicaodotologica/controller/ConsultaController.java
@@ -13,6 +13,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping ("/consulta")
 public class ConsultaController {
@@ -28,23 +31,24 @@ public class ConsultaController {
 
     @PostMapping ("/agendar")
     public ResponseEntity<?> agendarConsulta (@RequestBody Consulta consulta){
+        // work in progress : necessário de tratar o objeto consulta da resposta
 
         Cliente cliente =  daoCliente.findById(consulta.getCliente().getId()).get();
-        System.out.println(cliente.getNome());
-
         consulta.setNomePaciente(cliente.getNome());
         consulta.setEmailPaciente(cliente.getEmail());
         consulta.setEstado("Agendado");
 
         Dentista dentista =   daoDentista.findById(consulta.getDentista().getId()).get();
-
-        System.out.println(dentista.getNome());
-
         consulta.setNomeDentista(dentista.getNome());
 
         Consulta novaConsulta = daoConsulta.save(consulta);
 
-        return ResponseEntity.status(201).build();
+        Map<Object, Object> resposta = new HashMap<Object, Object>();
+
+        resposta.put("menssagem", "consulta agendada");
+        resposta.put("agendamento", novaConsulta);
+
+        return ResponseEntity.status(201).body(resposta);
 
     }
 
